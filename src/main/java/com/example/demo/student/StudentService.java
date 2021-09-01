bb00314f5a48fe1d903fdc70ca9fe8c3d9d85d7f
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-08-31 19:29:32 -0700

    student mvc

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+public class StudentService {
+
+    @GetMapping
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Mariam",
+                        "mariam.jamal@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+        );
+    }
+}
